4f458c51fbb1e8b7a5ad164fab86d1e043c93bec
Zsobrák Patrik <patrik@zsobrak-macbook-pro.local>
2019-01-02 11:36:07 +0100

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Zsobrák Patrik on 2019. 01. 02..
+//  Copyright © 2019. Zsobrák Patrik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
